b7aeaf15b9517e67e5ad081b906cb584d5938707
Mateusz Ogonek <ogonekmateusz69@gmail.com>
2026-03-12 18:20:51 +0100

    implement routing for MainPage, AppPage, LoginPage, and RegisterPage

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
+import MainPage from '../pages/MainPage'
+import AppPage from '../pages/AppPage'
+import LoginPage from '../pages/LoginPage'
+import RegisterPage from '../pages/RegisterPage'
 
 export default function AppRoutes() {
   return (
-    <div>AppRoutes</div>
+    <Routes>
+      <Route path="/" element={<MainPage />} />
+      <Route path="/app" element={<AppPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+    </Routes>
   )
 }
