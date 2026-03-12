f12fc520e9ea1fdb46a0aefa0e1febb994b7f1d2
Mateusz Ogonek <ogonekmateusz69@gmail.com>
2026-03-12 18:34:33 +0100

    feat: implement FormLayout component and update routing structure in AppRoutes

---
+++ src/layouts/FormLayout.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+
+export default function FormLayout() {
+  return (
+    <div>
+        fdsf
+      <Outlet />
+    </div>
+  );
+}
