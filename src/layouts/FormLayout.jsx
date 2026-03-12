af416a282b1bcfb8a2b010186ff62671dcfe04d5
Mateusz Ogonek <ogonekmateusz69@gmail.com>
2026-03-12 18:44:37 +0100

    fix: correct layout structure in FormLayout component

--- src/layouts/FormLayout.jsx
+++ src/layouts/FormLayout.jsx
@@ -3,8 +3,7 @@ import { Outlet } from "react-router-dom";
 
 export default function FormLayout() {
   return (
-    <div>
-        fdsf
+    <div className="w-full h-screen flex items-center justify-center bg-linear-to-tr from-blue-500/70 to-blue-600">
       <Outlet />
     </div>
   );
